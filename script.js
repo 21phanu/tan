60b3e30d387fe8ef5af46c9d988197abe8d1e49f
21phanu <phanuname3@gmail.com>
2025-02-22 18:55:47 +0700

    Update script.js

--- script.js
+++ script.js
@@ -11,8 +11,8 @@ noBtn.addEventListener("mouseover", () => {
 
 document.querySelector(".yes-btn").addEventListener("click", () => {
     document.querySelector(".container").innerHTML = `
-        <h1>เย้! 💕 เราเป็นแฟนกันแล้ว! 🥰</h1>
-        <p>ฉันจะทำให้คุณมีความสุขที่สุดเลยนะ 😊</p>
+        <h1>เย้! 💕 เราเป็นแฟนกันแล้วน้า 🥰</h1>
+        <p>เค้าจะทำให้คุณมีความสุขที่สุดเลยนะ ขอบคุณที่เข้ามาในชีวิต😊</p>
         <img src="https://media.giphy.com/media/3o7abAHdYvZdBNnGZq/giphy.gif" alt="Celebration" style="width: 200px; margin-top: 20px;">
     `;
 });
