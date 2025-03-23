83ce5f05ad6304a35db439df5a92e52178088ca8
21phanu <phanuname3@gmail.com>
2025-03-23 19:04:15 +0700

    Update script.js

--- script.js
+++ script.js
@@ -11,6 +11,5 @@ document.querySelector(".yes-btn").addEventListener("click", () => {
     document.querySelector(".container").innerHTML = `
         <h1>เย้! 💕 เราเป็นแฟนกันแล้วน้า 🥰</h1>
         <p>เค้าจะทำให้คุณมีความสุขที่สุดเลยนะ ขอบคุณที่เข้ามาในชีวิต😊</p>
-        <img src="https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExb3h6a2FlbzVxOWU4dzVmYjdpaWdvb29qbHAyMnl4NWN6amlwZDJlMCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/UVk5yzljef0kGiayL1/giphy.gif">
-    `;
+        <img src="https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExb3h6a2FlbzVxOWU4dzVmYjdpaWdvb29qbHAyMnl4NWN6amlwZDJlMCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/UVk5yzljef0kGiayL1/giphy.gif"> `;
 });
